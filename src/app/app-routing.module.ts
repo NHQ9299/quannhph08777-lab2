de05687e4ae82e0ec59169770f42f5d3788f945b
NHQ9299 <quannhph08777@fpt.edu.vn>
2020-03-23 15:04:11 +0700

    3 files modified, 6 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,23 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { AboutComponent } from './about/about.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ChiTietComponent } from './chi-tiet/chi-tiet.component';
 
 
 const routes: Routes = [
+  {path : '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'add-product', component: AddProductComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  {path :'services' , component :ServiceComponent},
+  {path : 'contact', component : ContactComponent},
+  { path: "product/add", component: AddProductComponent },
+  { path: "product/detail/:id", component: ChiTietComponent },
+  { path: "404", component: NotFoundComponent},
+  { path: "**", redirectTo: "404", pathMatch: "full" }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
@@ -22,7 +25,7 @@ import { AboutComponent } from './about/about.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, CategoryComponent, SlideComponent, TableComponent, ChiTietComponent, AddProductComponent, HomeComponent, AboutComponent,   ],
+  declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, CategoryComponent, SlideComponent, TableComponent, ChiTietComponent, AddProductComponent, HomeComponent, AboutComponent, ServiceComponent, ContactComponent, NotFoundComponent,   ],
   bootstrap:    [ AppComponent ],
   providers: [SService],
 })
